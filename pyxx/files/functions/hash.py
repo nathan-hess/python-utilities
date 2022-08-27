73532e72d7c54e70b27fe591ca67f3959008887c
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-08-27 15:44:29 -0400

    Create new module `pyxx.files` and add function to compute file hashes
    Set up new module `pyxx.files` to perform file I/O and processing, and added function `pyxx.files.compute_file_hash()`. Also added tests for new function

---
+++ pyxx/files/functions/hash.py
@@ -0,0 +1,54 @@
+"""
+This module contains functions for computing or analyzing file hashes.
+"""
+
+import hashlib
+import pathlib
+from typing import Union
+
+
+def compute_file_hash(file: Union[str, pathlib.Path],
+                      hash_function: str = 'sha256'):
+    """Computes a file hash
+
+    Computes and returns the hash of a file on the disk. Users can select
+    the hash function from those supported by ``hashlib``
+
+    Parameters
+    ----------
+    file : str or pathlib.Path
+        File whose hash is to be computed
+    hash_function : str, optional
+        Hash function, selected from the options described in the
+        "Notes" section (default is ``'sha256'``)
+
+    Returns
+    -------
+    str
+        Name of hash function (in the format used by ``hashlib``) which
+        was computed
+    str
+        Hash of ``file`` as computed by the hash function specified
+        by ``hash_function``
+
+    Notes
+    -----
+    Any hash function included in Python's ``hashlib`` module can be used,
+    as described here: https://docs.python.org/3/library/hashlib.html.
+
+    Inputs for the ``hash_function`` argument are not case-sensitive, and
+    dashes (``-``) and underscores (``_``) can be used interchangeably, or
+    omitted.  Thus, to use the SHA-256 hash function, any of the following
+    are valid inputs: ``'sha256'``, ``'SHA-256'``, ``'sha_256'``.
+    """
+    # Set hash function
+    hash_name = hash_function.lower().replace('-', '').replace('_', '')
+    file_hash_function = getattr(hashlib, hash_name)
+
+    # Compute file hash
+    file_hash: hashlib._Hash = file_hash_function()
+    with open(file, 'rb') as fileID:
+        while block := fileID.read(file_hash.block_size):
+            file_hash.update(block)
+
+    return (hash_name, file_hash.hexdigest())
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,12 @@
+import pathlib
+
+
+# Define variables available to all tests
+PROJECT_TEST_DIR = pathlib.Path(__file__).resolve().parent
+SAMPLE_FILES_DIR = PROJECT_TEST_DIR / 'sample_files'
+TEST_TMP_DIR = PROJECT_TEST_DIR / 'tmp'
+
+
+# Import and run tests
+from .files import *
 from .strings import *
