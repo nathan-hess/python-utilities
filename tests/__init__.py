8a7e3511c16ced7e1b6753f68d0365dc603a9c5a
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-08-27 16:12:02 -0400

    Change name of folder with sample files for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,7 @@ import pathlib
 
 # Define variables available to all tests
 PROJECT_TEST_DIR = pathlib.Path(__file__).resolve().parent
-SAMPLE_FILES_DIR = PROJECT_TEST_DIR / 'sample_files'
+SAMPLE_FILES_DIR = PROJECT_TEST_DIR / '_sample_files'
 TEST_TMP_DIR = PROJECT_TEST_DIR / 'tmp'
 
 
