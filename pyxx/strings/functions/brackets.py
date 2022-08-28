07254862f28a8cc80c79e2bd68bb21e9bb378f36
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-08-27 22:35:20 -0400

    Add code coverage exclusion
    Added code coverage exclusion for a line that should never be run (but is kept as a backup precaution)

--- pyxx/strings/functions/brackets.py
+++ pyxx/strings/functions/brackets.py
@@ -274,7 +274,7 @@ def find_skip_brackets(value: str, target_chars: Union[str, tuple],
                 # This error should never be thrown since the string was
                 # previously check for matching brackets; however, it's
                 # included as an added precaution
-                raise UnmatchedBracketsError(
+                raise UnmatchedBracketsError(   # pragma: no cover
                     f'Unmatched brackets found in string "{value}"')
 
         # Advance to next character in string
