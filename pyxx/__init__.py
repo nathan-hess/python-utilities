4e2a9543a21b8348cc6edea7a1cf9a7dfad4cfcd
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-09-24 12:19:01 -0400

    Simplify program version format in `pyxx/__init__.py`

--- pyxx/__init__.py
+++ pyxx/__init__.py
@@ -6,8 +6,4 @@ from . import strings
 
 
 # PROGRAM VERSION ------------------------------------------------------------
-_VERSION_MAJOR = 1
-_VERSION_MINOR = 0
-_VERSION_PATCH = 0
-
-__version__ = f'{_VERSION_MAJOR}.{_VERSION_MINOR}.{_VERSION_PATCH}'
+__version__ = '1.0.0'
