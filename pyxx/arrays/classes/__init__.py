0caa74aad9ef3bcdf003a9a1a128f09a3764c248
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-10-30 22:48:37 -0400

    Create `pyxx.arrays.TypedList` and `pyxx.arrays.TypedListWithID` classes
    Created new classes which function in a similar manner as built-in Python lists, but require that all list items be of a user-specified type. Also added unit tests for these new classes

---
+++ pyxx/arrays/classes/__init__.py
@@ -0,0 +1,6 @@
+"""This module contains classes that provide nearly identical functionality
+as built-in Python :py:class:`list` objects, but which require that all items
+in a list be of a specified type
+"""
+
+from .typedlist import TypedList, TypedListWithID
