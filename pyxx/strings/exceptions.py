adddd37908b6c608b744bac8c683c03f697be0f7
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-11-16 10:57:46 -0500

    Update Flake8 configuration
    Modified Flake8 configuration to allow fewer than 2 blank lines before class definitions in `exceptions.py` files. These files store only classes representing customized exceptions, so allowing the lines to be more closely spaced reduces file length and improves readability

--- pyxx/strings/exceptions.py
+++ pyxx/strings/exceptions.py
@@ -5,11 +5,9 @@
 class BracketErrors(Exception):
     """Errors for the ``pyxx.strings.brackets`` module"""
 
-
 class NotABracketError(BracketErrors, ValueError):
     """Error thrown if encountering a non-bracket character when
     expecting to encounter a bracket character"""
-
 
 class UnmatchedBracketsError(BracketErrors, ValueError):
     """Error thrown if unmatched brackets are found in an
